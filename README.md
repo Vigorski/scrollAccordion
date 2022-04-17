c91a080889b4aa1a6313c73f1838222bdb36aef7
Igor Veleski <vele.igor@gmail.com>
2022-04-17 22:42:41 +0200

    #170422 initial setup with RC/2.0.0

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
-# scrollAccordion
-A simple accordion for annoyingly long content
+# scrollAccordion 2.0.0
+
+scrollAccordion is a very simple JavaScript plug-in intended to handle annoyingly long content in a more pleasant and user friendly manner.
+
+If your website has a collection of items with very long content, instead of landing in the middle of the newly expanded item and losing track of your position, scrollAccordion will, you guessed it, scroll to the very beginning of each expanded item.
+
+Each call of scrollAccordion creates a separate instance and will work independently of other instances. So, you could set up multiple accordions with their own content, options and toggle buttons and they will not interfere with each other.
+
+<br>
+
+### CDN
+If you don't want to download it, you can use my shabby CDNs:
+- https://vigorski.github.io/scrollAccordion/scrollAccordion.js
+- https://vigorski.github.io/scrollAccordion/scrollAccordion.min.js
+
+
+### Setup
+Using scrollAccordion is very easy.
+
+1. Use CDN or download the regular or minimal version of the plugin and place the file right before the closing body tag.
+    ```html
+    ...
+    <script src="https://vigorski.github.io/scrollAccordion/scrollAccordion.js"></script>
+    </body>
+    ```
+2. Set up the structure. While not too restricted, it still needs to follow some rules. This is the bare minimum necessary for the plugin to work:
+
+    ```html
+    <ul class="scroll-accordion">
+      <li>
+        <div class="scroll-accordion-header">...</div>
+        <div class="scroll-accordion-body">...</div>
+      </li>
+      ...
+    </ul>
+    ```
+
+    The 'scroll-accordion' class is used only for naming continuity, but is otherwise optional.
+
+    In terms of style, it is not necessary to include anything for the plugin to work, but I recommend you include this one line in order to avoid the jumping of elements during load times while they are being hidden:
+
+    ```css
+    .scroll-accordion-body-wrapper { display: none; }
+    ```
+
+3. Select the element whos children will be converted into an accordion:
+    ```js
+    $('.scroll-accordion').scrollAccordion({options});
+    ```
+4. Sit back and relax.
+
+<br>
+
+### Settings
+You can change the behaviour of the accordion with the following optional properties:
+
+Option | Type | Default | Description
+------ | ---- | ------- | -----------
+htmlContainer | DOM Element | 'html, body' | Select custom container for the accordion. Useful for small overflowing containers where you want the scrolling to be bound to the custom element instead of the body of the document (Always returns a jQuery object).
+animationSpeed | number | 300 | Set speed of animation in ms. (Enter 0 if you don't want it animated).
+scrollToSelection | boolean | true | Toggle option to follow and scroll to the top of expanded/collapsed item or remain stationary.
+scrollGap | number | 10 | Set distance between top border of window and top border of clicked accordion item.
+multiExpanded | boolean | false | Toggle expanding multiple items
+initialAllExpanded | boolean | false | Toggle whether to load page with all accordion items expanded or not.
+expandAllSource | DOM Element | null | Select button switch used to expand/collapse all accordion items from a particular instance.
+expandAllBtnContent | Array | null | Set the two states that the 'expandAllSource' target will toggle between. For security reasons, it is not allowed to include a DOM Element. The valid data to be provided is the following: Array of two objects. Each object must have at least a 'tags' property and an optional 'classNames' and 'texts' properties. The value of these properties must be an array with same length as the others. If a property does not need a value in a specific array spot, simply provide an empty string. See demo for a demonstration of this property.
+active | number | null | Specify to have an expanded item on page load (starts from 0).
+
+---
+
+<br>
+
+#### Check out the demo:
+[https://codepen.io/Vigorski/pen/Pozvgqy](https://codepen.io/Vigorski/pen/Pozvgqy)
+
+<br>
+
+#### Dependencies
+
+jQuery 3.4
+
+<br>
+
+
+---
+#### Author: Igor Veleski
+
+---
+
+#### RC/2.0.0 
+##### (Released 17.04.22)
---
+++ src/scrollAccordion.js
@@ -0,0 +1,483 @@
+'use strict';
+
+(function ($) {
+	const v = {
+		isDefined: function(value) {
+			return value !== null && value !== undefined;
+		},
+		isNumber: function(value){
+			return typeof value === 'number' && !isNaN(value);
+		},
+		isString: function(value) {
+			return typeof value === 'string' || value instanceof String;
+		},
+		isBoolean: function(value){
+			return typeof value === 'boolean';
+		},
+		isObject: function(obj){
+			return obj === Object(obj);
+		},
+		isJqueryElement: function(ele) {
+			return typeof ele !== 'undefined' && ele instanceof jQuery;
+		},
+		isDomElement: function(ele) {
+			if (!ele) {
+				return false;
+			}
+
+			if (
+				ele instanceof Node ||
+				ele instanceof NodeList ||
+				ele instanceof HTMLCollection
+			) {
+				return true;
+			}
+
+			if (v.isObject(document) && ele === document) {
+				return true;
+			}
+		},
+		isScriptTag: function(ele) {
+			if (ele instanceof HTMLScriptElement) {
+				return true;
+			}
+		},
+		isEmpty: function(value) {
+			// Null and undefined are empty
+			if (!v.isDefined(value)) {
+				return true;
+			}
+
+			// Using the length property for arrays
+			if (Array.isArray(value)) {
+				return value.length === 0;
+			}
+			
+			// Not empty if DOM Node
+			if (value instanceof Node) {
+				return false;
+			}
+
+			// Not empty if jQuery object or DOM Element
+			if (v.isJqueryElement(value) || v.isDomElement(value)){
+				for (const attr of value) {
+					return false;
+				}
+				return true
+			}
+			
+			// Not empty if there is at least one property
+			if (v.isObject(value)) {
+				for (const attr in value) {
+					return false;
+				}
+				return true;
+			}
+
+			return false;
+		}
+	}
+
+	$.fn.scrollAccordion = function (options = {}) {
+		const T_ACCORDION_ACTIVE = "scroll-accordion--active";
+		const T_ACCORDION_HEADER = ".scroll-accordion-header-wrapper";
+		const T_ACCORDION_HEADER_INNER = ".scroll-accordion-header";
+		const T_ACCORDION_BODY = ".scroll-accordion-body-wrapper";
+		const T_ACCORDION_BODY_INNER = ".scroll-accordion-body";
+		
+		const isValid = {
+			htmlContainer: false,
+			animationSpeed: false,
+			scrollToSelection: false,
+			scrollGap: false,
+			multiExpanded: false,
+			expandAllSource: false,
+			initialAllExpanded: false,
+			expandAllBtnContent: false,
+			active: false,
+		};
+
+		_validateOptions();
+
+		const setup = {
+			accordionHeaders: [],
+			activeAccordionItem: null,
+			activeAccordionItemPosition: null,
+			activeAccordionItemContentHeight: null,
+			activeAccordionItemHeaderHeight: null,
+			queuedAccordionItemPosition: null,
+			scrollToTargetPosition: null,
+		};
+		
+		// VALIDATED OPTIONS
+		const accordionOptions = {
+			htmlContainer: isValid.htmlContainer ? options.htmlContainer : $("body, html"),
+			animationSpeed: isValid.animationSpeed ? options.animationSpeed : 300,
+			scrollToSelection: isValid.scrollToSelection ? options.scrollToSelection : true,
+			scrollGap: isValid.scrollGap ? options.scrollGap : 10,
+			multiExpanded: isValid.multiExpanded ? options.multiExpanded : false,
+			expandAllSource: null,
+			initialAllExpanded: isValid.initialAllExpanded ? options.initialAllExpanded : false,
+			expandAllBtnContent: null,
+			active: null,
+		};
+
+		_init(this);
+
+		/////////////////////////////////////
+		// ACCORDION VALIDATION
+		/////////////////////////////////////
+
+		function _validateOptions(_this) {
+			// htmlContainer, expandAllSource
+			const DOMElementOptions = ['htmlContainer', 'expandAllSource'];
+			for(const option of DOMElementOptions) {
+				if ( !v.isEmpty(options[option]) ) {
+					if (
+						!v.isScriptTag(options[option]) &&
+						(v.isJqueryElement(options[option]) || v.isDomElement(options[option]) )
+					){
+						isValid[option] = true;
+					} else {
+						throw (new Error(
+							`Invalid argument '${option}'`,
+							{cause: `Function argument '${option}' must be a valid DOM element!`}
+						))
+					}
+				}
+			}
+			
+			// animationSpeed, scrollGap, active
+			const numberOptions = ['animationSpeed', 'scrollGap', 'active'];
+			for(const option of numberOptions) {
+				if ( !v.isEmpty(options[option]) ) {
+					if ( v.isNumber(options[option]) && options[option] > 0 ) {
+						isValid[option] = true;
+					} else {
+						throw (new Error(
+							`Invalid argument '${option}'`,
+							{cause: `Function argument '${option}' must be a valid greater than zero number!`}
+						))
+					}
+				}
+			}
+
+			// scrollToSelection, multiExpanded, initialAllExpanded
+			const booleanOptions = ['scrollToSelection', 'multiExpanded', 'initialAllExpanded'];
+			for(const option of booleanOptions) {
+				if ( !v.isEmpty(options[option]) ) {
+					if ( v.isBoolean(options[option]) ) {
+						isValid[option] = true;
+					} else {
+						throw (new Error(
+							`Invalid argument '${option}'`,
+							{cause: `Function argument '${option}' must be a boolean value!`}
+						))
+					}
+				}
+			}
+
+			// expandAllBtnContent
+			if (!v.isEmpty(options.expandAllBtnContent)) {
+				// first check: is array and has exactly 2 items
+				if( Array.isArray(options.expandAllBtnContent) && options.expandAllBtnContent.length === 2 ) {
+					options.expandAllBtnContent.forEach((item) => {
+						// no script tags allowed
+						for(let i = 0; i < item.tags.length; i++) {
+							if (item.tags[i] === 'script') {
+								throw(new Error(
+									`Unpermitted node 'script'`,
+									{cause: 'Script tags are not allowed!'}
+								));
+							}
+							if (!v.isString(item.tags[i])){
+								throw(new Error(
+									`Invalid argument value 'expandAllBtnContent[].tags'`,
+									{cause: `'tags' property must be a string, albeit empty`}
+								));
+							}
+						}
+						
+						// next check: all array items inside the object must have the same length
+						// structure must be an array with 2 objects which have three keys with arrays as value
+						// i.e: [Array] >> 2x {object} >> key: [Array]
+						// comparing each array against the first one (which is required)
+						if (Object.values(item).every(currentValue => currentValue.length === item.tags.length)){
+							isValid.expandAllBtnContent = true;
+						} else {
+							throw(new Error(
+								`Invalid argument 'expandAllBtnContent'`,
+								{cause: 'Object properties for expand button must be arrays all of which must have the exact same number of items'}
+							));
+						}
+					});
+				} else {
+					throw (new Error(
+						`Invalid argument 'expandAllBtnContent'`,
+						{cause: `Function argument 'expandAllBtnContent' must be an array!`}
+					))
+				}
+			}
+		}
+
+		/////////////////////////////////////
+		// END OF ACCORDION VALIDATION
+		/////////////////////////////////////
+
+		/////////////////////////////////////
+		// ACCORDION INITIALIZATION
+		/////////////////////////////////////
+
+		function setHeaderAndBody (_this) {
+			// wrap immediate header and body of accordion item with another div
+			// to prevent margin leaking and wrong expand/collapse animations
+			const wrappedHeaders = _this.map(function(i, accordion){
+				const firstHeaderChild = $(accordion).children('li').children(T_ACCORDION_HEADER_INNER);
+				const firstBodyChild = $(accordion).children('li').children(T_ACCORDION_BODY_INNER);
+				
+				if (firstBodyChild.length) {
+					firstBodyChild.wrap('<div class="scroll-accordion-body-wrapper"/>').parent().css({display: 'none'});
+				}
+	
+				if (firstHeaderChild.length) {
+					const wrapHeader = firstHeaderChild.wrap('<div class="scroll-accordion-header-wrapper"/>').parent();
+					return [...wrapHeader];
+				}
+			});
+
+			if (wrappedHeaders.length) {
+				setup.accordionHeaders = wrappedHeaders;
+			} else {
+				throw (new Error(
+					'Invalid accordion header elements',
+					{cause: `Accordion must have at least one valid header DOM Element`}
+				))
+			}
+		}
+
+		function setBaseContainer() {
+			// set a mandatory jQuery object of base or custom container
+			if (isValid.htmlContainer) {
+				accordionOptions.htmlContainer = v.isJqueryElement(accordionOptions.htmlContainer) ? accordionOptions.htmlContainer : $(accordionOptions.htmlContainer);
+			}
+		}
+
+		function setActiveItem() {
+			if(isValid.active && options.active < setup.accordionHeaders.length) {
+				accordionOptions.active = options.active;
+			}
+		}
+
+		function setExpandAllSource() {
+			if (isValid.expandAllSource) {
+				accordionOptions.expandAllSource = v.isJqueryElement(options.expandAllSource) ? options.expandAllSource : $(options.expandAllSource)
+			}
+		}
+
+		function setExpandAllSwitch() {
+			if (isValid.expandAllBtnContent) {
+				accordionOptions.expandAllBtnContent = options.expandAllBtnContent.map((item) => {
+					// construct the expand btn elements
+					const currentSetOfBtnElements = [];
+
+					for(let i = 0; i < item.tags.length; i++) {
+						const eleTag = item.tags[i].trim();
+						if (eleTag.length === 0) continue;
+						const eleClass = item.classNames[i].trim();
+						const eleText = item.texts[i].trim();
+						const ele = document.createElement(eleTag);
+
+						if (eleClass.length) {
+							ele.classList.add(...eleClass.split(' '));
+						}
+						
+						if (eleText.length) {
+							const eleTextNode = document.createTextNode(eleText)
+							ele.appendChild(eleTextNode);
+						}
+
+						currentSetOfBtnElements.push(ele);
+					}
+
+					return $(currentSetOfBtnElements);
+				});
+			}
+		}
+
+		function _init(_this)  {
+			setHeaderAndBody(_this);
+			setBaseContainer();
+			setActiveItem();
+			setExpandAllSource();
+			setExpandAllSwitch();
+		}
+		
+		/////////////////////////////////////
+		// END OF ACCORDION INITIALIZATION
+		/////////////////////////////////////
+
+		/////////////////////////////////////
+		// ACCORDION CONTOLLERS
+		/////////////////////////////////////
+
+		function getItemOffset (_this) {
+			const itemOffset = $(_this).offset().top;
+			// if customContainer > only calculate the offset from parent, else > from top of screen
+			if(isValid.htmlContainer) {
+				const customHtmlContainerOffset = accordionOptions.htmlContainer.offset().top;
+				const customHtmlContainerScroll = accordionOptions.htmlContainer[0].scrollTop;
+				return ( itemOffset - customHtmlContainerOffset ) + customHtmlContainerScroll;
+			}
+
+			return itemOffset;
+		}
+
+		function getQueuedAccItemPosition(_this) {
+			const clickedItemOffset = Math.floor( getItemOffset(_this) );
+			setup.queuedAccordionItemPosition = clickedItemOffset - accordionOptions.scrollGap;
+		}
+		
+		function getActiveAccItemPosition(_this) {
+			if (accordionOptions.multiExpanded) return;
+
+			setup.activeAccordionItem = $(_this).parent().parent().find(`.${T_ACCORDION_ACTIVE}`);
+			
+			if (setup.activeAccordionItem.length === 0) {
+				setup.activeAccordionItemPosition = setup.queuedAccordionItemPosition;
+				return;
+			}
+	
+			const activeItemOffset = Math.floor( getItemOffset(setup.activeAccordionItem[0]) );
+			setup.activeAccordionItemPosition = activeItemOffset - accordionOptions.scrollGap;
+		}
+
+		function getActiveAccItemHeight() {
+			if (accordionOptions.multiExpanded) return;
+
+			setup.activeAccordionItemHeaderHeight = Math.floor( setup.activeAccordionItem.children(T_ACCORDION_HEADER).outerHeight() );
+			setup.activeAccordionItemContentHeight = Math.floor(
+				setup.activeAccordionItem.outerHeight() - setup.activeAccordionItemHeaderHeight 
+			);
+		}
+
+		function setScrollPosition() {
+			if (!accordionOptions.scrollToSelection) return;
+
+			// if clicked item is positioned after currently active item > 
+			// remove the height of the active item
+			// from the queued scrolling position so the scroll will be correct
+			if (setup.queuedAccordionItemPosition > setup.activeAccordionItemPosition) {
+				setup.scrollToTargetPosition = setup.queuedAccordionItemPosition - setup.activeAccordionItemContentHeight;
+				return;
+			}
+
+			setup.scrollToTargetPosition = setup.queuedAccordionItemPosition;
+		}
+
+		function toggleAllAccordion() {
+			accordionOptions.initialAllExpanded ? collapseAll(setup.accordionHeaders) : expandAll(setup.accordionHeaders);
+			
+			if(isValid.expandAllBtnContent) {
+				accordionOptions.expandAllSource.html(accordionOptions.expandAllBtnContent[accordionOptions.initialAllExpanded ? 1 : 0]);
+			}
+		}
+
+		function toggleAccordion($accHeader) {
+			// remove active accordion item if not multi
+			if (
+				!accordionOptions.multiExpanded &&
+				!v.isEmpty(setup.activeAccordionItem)
+			) {
+				const sameActiveItem = $accHeader.parent().hasClass(T_ACCORDION_ACTIVE);
+
+				$accHeader
+					.parent()
+					.parent()
+					.children(`.${T_ACCORDION_ACTIVE}`)
+					.removeClass(T_ACCORDION_ACTIVE)
+					.children(T_ACCORDION_BODY)
+					.slideUp(accordionOptions.animationSpeed)
+
+				// skip next step if clicking on already active item
+				if (sameActiveItem) return;
+			}
+
+			// Toggle single/multi
+			$accHeader
+				.parent()
+				.toggleClass(T_ACCORDION_ACTIVE)
+				.children(T_ACCORDION_BODY)
+				.slideToggle(accordionOptions.animationSpeed);
+		}
+
+		function scrollScreenToPosition() {
+			// Set screen at top of opened item
+			if (accordionOptions.scrollToSelection) {
+				accordionOptions.htmlContainer.animate({
+					scrollTop: setup.scrollToTargetPosition
+				}, accordionOptions.animationSpeed);
+			}
+		}
+
+		function collapseAll($accHeader) {
+			accordionOptions.expandAllSource.removeClass('expanded-all');
+			accordionOptions.initialAllExpanded = false;
+			$accHeader
+				.parent()
+				.removeClass(T_ACCORDION_ACTIVE)
+				.children(T_ACCORDION_BODY)
+				.slideUp(accordionOptions.animationSpeed);
+		}
+
+		function expandAll($accHeader) {
+			accordionOptions.expandAllSource.addClass('expanded-all');
+			accordionOptions.initialAllExpanded = true;
+			$accHeader
+				.parent()
+				.addClass(T_ACCORDION_ACTIVE)
+				.children(T_ACCORDION_BODY)
+				.slideDown(accordionOptions.animationSpeed);
+		}
+
+		/////////////////////////////////////
+		// END OF ACCORDION CONTOLLERS
+		/////////////////////////////////////
+		
+		/////////////////////////////////////
+		// ACCORDION EVENTS AND SET INITIAL STATE
+		/////////////////////////////////////
+
+		setup.accordionHeaders.on("click", function () {
+			// CALCULATE POSITION, PROPORTIONS AND OFFSETS
+			getQueuedAccItemPosition(this)
+			getActiveAccItemPosition(this);
+			getActiveAccItemHeight();
+			setScrollPosition();
+			toggleAccordion($(this));
+			scrollScreenToPosition();
+		});
+
+		if (isValid.expandAllSource) {
+      accordionOptions.expandAllSource.on("click", function () {
+        toggleAllAccordion();
+      });
+    }
+
+		// Open acc item if active is valid
+		if (accordionOptions.active !== null) {
+			toggleAccordion($(setup.accordionHeaders[accordionOptions.active]));
+			// reset active acc item from options
+			accordionOptions.active = null;
+		}
+
+		if (accordionOptions.initialAllExpanded) {
+			expandAll($(setup.accordionHeaders));
+		}
+
+		/////////////////////////////////////
+		// END OF ACCORDION EVENTS AND SET INITIAL STATE
+		/////////////////////////////////////
+
+		return this;
+	}
+}(jQuery));
